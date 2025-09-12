1268cb27b74744e343152dfeed9e84ac50ed8a86
Juck Fuck <juckfuck@duck.com>
2025-09-12 19:48:49 +0100

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
-import type { Metadata } from 'next'
-import './globals.css'
-
+import type { Metadata } from "next";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 export const metadata: Metadata = {
-  title: 'Wibbly Wobblaz',
-  description: 'A sound and cultural experiment, gone wobbly.',
-}
+  title: "Wibbly Wobblaz",
+  description: "A sound and cultural experiment, gone wobbly.",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body suppressHydrationWarning={true}>{children}</body>
     </html>
-  )
+  );
 }
